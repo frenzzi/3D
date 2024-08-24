4a61b9833f29f3fd1ffa4225aab9dde8366e030d
frenzzi <MrChiter3@yandex.ru>
2024-08-24 15:11:11 +0700

    change classes

--- Assets/Scripts/MoverToForward.cs
+++ Assets/Scripts/MoverToForward.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveForward : MonoBehaviour
+public class MoverToForward : MonoBehaviour
 {
     [SerializeField] private Vector3 _movementDirection;
 
-    void Update()
+    private void Update()
     {
         transform.Translate(_movementDirection, Space.Self);
     }
--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scale : MonoBehaviour
+public class Scaler : MonoBehaviour
 {
     [SerializeField] private Vector3 _maxScale;
     [SerializeField] private Vector3 _scalingSpeed;

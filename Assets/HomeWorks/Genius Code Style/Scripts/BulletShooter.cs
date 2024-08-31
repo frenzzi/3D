bcd50f48e65027f8bdbc0bf127a3ab9c04a2905a
frenzzi <MrChiter3@yandex.ru>
2024-08-31 14:20:47 +0700

    Гениальный Гений

---
+++ Assets/HomeWorks/Genius Code Style/Scripts/BulletShooter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+namespace CodeStyleGenuis
+{
+    public class BulletShooter : MonoBehaviour
+    {
+        [SerializeField, Min(0)] private float _speed = 1.0f;
+        [SerializeField, Min(0)] private float _timeToShoot = 1.0f;
+        [SerializeField] private Rigidbody _bulletPrefab;
+        [SerializeField] private Transform _bulletTarget;
+
+        void Start()
+        {
+            StartCoroutine(Shooting());
+        }
+
+        private IEnumerator Shooting()
+        {
+            while (isActiveAndEnabled)
+            {
+                var direction = (_bulletTarget.position - transform.position).normalized;
+
+                var newBullet = Instantiate(_bulletPrefab, transform.position + direction, Quaternion.identity);
+
+                newBullet.transform.up = direction;
+                newBullet.velocity = direction * _speed;
+
+                yield return new WaitForSeconds(_timeToShoot);
+            }
+        }
+    }
+}
+
---
+++ Assets/HomeWorks/Genius Code Style/Scripts/MoverByPoints.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace CodeStyleGenuis
+{
+    public class MoverByPoints : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private Transform _placesParent;
+
+        private Transform[] _places;
+        private int _numberOfCurrentPlace = 0;
+
+        private void Start()
+        {
+            if (_placesParent == null || _placesParent.childCount == 0)
+                return;
+
+            _places = new Transform[_placesParent.childCount];
+
+            for (int i = 0; i < _placesParent.childCount; i++)
+            {
+                _places[i] = _placesParent.GetChild(i);
+            }
+
+            if (_places.Length > 0)
+            {
+                Vector3 direction = _places[_numberOfCurrentPlace].position - transform.position;
+                transform.forward = direction.normalized;
+            }
+        }
+
+        private void Update()
+        {
+            if (_places == null || _places.Length == 0)
+                return;
+
+            Transform currentTarget = _places[_numberOfCurrentPlace];
+
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, _speed * Time.deltaTime);
+
+            if (transform.position == currentTarget.position)
+                UpdateNextTarget();
+        }
+
+        private void UpdateNextTarget()
+        {
+            _numberOfCurrentPlace++;
+
+            if (_numberOfCurrentPlace >= _places.Length)
+            {
+                _numberOfCurrentPlace = 0;
+            }
+
+            Vector3 direction = _places[_numberOfCurrentPlace].position - transform.position;
+            transform.forward = direction.normalized;
+        }
+    }
+}
+
